bdfc870b08485a73e1b9cab34d4227910028b1e2
grankko <grankko@gmail.com>
2018-05-12 16:37:13 +0200

    its a start

---
+++ BlazorLife/BlazorLife.Game.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BlazorLife.Game.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var sut = new BlazorLife.Game.Class1();
+            Assert.IsNotNull(sut);
+        }
+    }
+}
---
+++ BlazorLife/BlazorLife.Game/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BlazorLife.Game
+{
+    public class Class1
+    {
+    }
+}
