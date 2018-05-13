aeec2bf2228cfd584d7b4ea9fd7331d5bf52c314
grankko <grankko@gmail.com>
2018-05-13 16:49:12 +0200

    fixed StateHasChanged > OnAfterRender loop

---
+++ BlazorLife/BlazorLife.Client/Interop/GenericFunctions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Blazor.Browser.Interop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorLife.Client.Interop
+{
+    public static class GenericFunctions
+    {
+        public static void UpdateElementInnerHtml(string id, string html)
+        {
+            RegisteredFunction.Invoke<bool>("updateElementInnerHtml", id, html);
+        }
+    }
+}
